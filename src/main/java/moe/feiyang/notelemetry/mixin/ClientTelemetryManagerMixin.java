31c338bb5a8ad594ad8d0f09ad7ab3b996aeedb9
Fei Yang <me@feiyang.moe>
2021-09-25 07:14:11 +0000

    Bug fixes.

--- src/main/java/moe/feiyang/notelemetry/mixin/ClientTelemetryManagerMixin.java
+++ src/main/java/moe/feiyang/notelemetry/mixin/ClientTelemetryManagerMixin.java
@@ -2,17 +2,14 @@ package moe.feiyang.notelemetry.mixin;
 
 import net.minecraft.client.ClientTelemetryManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Overwrite;
 import moe.feiyang.notelemetry.ModMain;
 
 @Mixin(ClientTelemetryManager.class)
 public class ClientTelemetryManagerMixin  {
-    @Inject(at = @At("HEAD"), method = "sendWorldLoadEvent")
+    @Overwrite
     //Keep vanilla params but do nothing
-    private void sendWorldLoadEventMixin(ClientTelemetryManager.PlayerInfo r, CallbackInfo ci) {
+    private void sendWorldLoadEvent(ClientTelemetryManager.PlayerInfo pi) {
         ModMain.LOGGER.info("[NoTelemetry] World Load event is cancelled.");
-        return;
     }
 }
